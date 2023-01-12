37c9bbb72b8094f0733033dc1ba41688235e26cb
Najmus Sakib <nazmusakib412@gmail.com>
2023-01-13 02:47:51 +0600

    update: default jwt secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  SECRET_KEY = ENV.fetch('JWT_SECRET_KEY') { Rails.application.secrets.secret_key_base.to_s }
+  SECRET_KEY = ENV.fetch('JWT_SECRET_KEY') { 'my_secret_key' }
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i

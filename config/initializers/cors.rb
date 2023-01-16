8ee99675d5a8911790878bf61a99a1c54b4830f2
Najmus Sakib <nazmusakib412@gmail.com>
2023-01-16 22:20:16 +0600

    add: local origin for dev

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "rescript-task.vercel.app"
+    origins 'rescript-task.vercel.app', 'localhost:3001'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
